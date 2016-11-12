5414b0da52ba54384e29cf77eda8ee01d652fe20
Tangi Vass <tangi.vass.ep@biznet.io>
2016-11-13 00:51:47 +0100

    Adding battery.py patch

--- script/python3-patch.sh
+++ script/python3-patch.sh
@@ -1,4 +1,6 @@
-cd "`python -c 'import site; print(site.getsitepackages()[0])'`/expfactory/"
+#cd "`python -c 'import site; print(site.getsitepackages()[0])'`/expfactory/"
+cd /opt/conda/lib/python3.5/site-packages/expfactory/
+
 find . -name "*.py" | xargs sed -i -e 's/iteritems/items/g'
 find . -name "*.py" | xargs sed -i -e 's/print \(.*\)$/print(\1)/'
 find . -name "*.py" | xargs sed -i -e 's/SimpleHTTPServer/http.server/'
@@ -13,3 +15,5 @@ sed -i -e '84s/"rb"/"r"/' utils.py
 sed -i -e 's/basestring/str/' utils.py
 
 sed -i -e 's/ \([^ ]*\.keys()\)\(\[.*\]\)/ list(\1)\2/' battery.py
+
+sed -i -e '43 a \ \ \ \ valid_games = []' battery.py # bug fix
